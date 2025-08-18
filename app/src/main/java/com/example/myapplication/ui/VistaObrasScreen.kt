efd00e9a070e14f7c7431e1090079821284d750f
jkkhumbgham <juanalbac06@gmail.com>
2025-08-18 13:09:30 -0500

    x

--- app/src/main/java/com/example/myapplication/ui/VistaObrasScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/VistaObrasScreen.kt
@@ -11,14 +11,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
