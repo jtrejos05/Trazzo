f922e4762350c470029a89b83ea3c6c65193bdf6
jkkhumbgham <juanalbac06@gmail.com>
2025-09-02 18:23:52 -0500

    .

--- app/src/main/java/com/example/myapplication/ui/Trending/TrendingViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/Trending/TrendingViewModel.kt
@@ -2,7 +2,6 @@ package com.example.myapplication.ui.Trending
 
 import androidx.lifecycle.ViewModel
 import com.example.myapplication.data.local.ProveedorObras
-import com.example.myapplication.ui.PricipalState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
 
