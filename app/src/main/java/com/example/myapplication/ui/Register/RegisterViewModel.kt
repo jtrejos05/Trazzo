6d323586b657cc862bf2133fffc12227f755e41e
jkkhumbgham <juanalbac06@gmail.com>
2025-09-15 15:32:18 -0500

    a

--- app/src/main/java/com/example/myapplication/ui/Register/RegisterViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/Register/RegisterViewModel.kt
@@ -18,61 +18,69 @@ class RegisterViewModel @Inject constructor(
     private val _uiState = MutableStateFlow(RegisterState())
     var uiState: MutableStateFlow<RegisterState> = _uiState
 
-    fun registerButtonPressed(){
-        if (_uiState.value.contraseña.isNullOrEmpty() ||_uiState.value.correo.isNullOrEmpty() ||_uiState.value.usuario.isNullOrEmpty() ||_uiState.value.edad.isNullOrEmpty()){
-           _uiState.update { it.copy(mostrarMensajeError = true) }
-           _uiState.update { it.copy(mensajeError = "Debes llenar al menos los campos: Correo, Contraseña, Usuario y Edad ") }
-        }else{
-            if (_uiState.value.contraseña.length<6){
-               _uiState.update { it.copy(mostrarMensajeError = true) }
-               _uiState.update { it.copy(mensajeError = "La contraseña debe tener al menos 6 caracteres ") }
-            }else{
-                if (_uiState.value.correo == "admin@admin.com"){
-                   _uiState.update { it.copy(mostrarMensajeError = true) }
-                   _uiState.update { it.copy(mensajeError = "Ese correo ya esta en uso ") }
-                }
-                else{
+    fun registerButtonPressed() {
+        if (_uiState.value.contraseña.isNullOrEmpty() || _uiState.value.correo.isNullOrEmpty() || _uiState.value.usuario.isNullOrEmpty() || _uiState.value.edad.isNullOrEmpty()) {
+            _uiState.update { it.copy(mostrarMensajeError = true) }
+            _uiState.update { it.copy(mensajeError = "Debes llenar al menos los campos: Correo, Contraseña, Usuario y Edad ") }
+        } else {
+            if (_uiState.value.contraseña.length < 6) {
+                _uiState.update { it.copy(mostrarMensajeError = true) }
+                _uiState.update { it.copy(mensajeError = "La contraseña debe tener al menos 6 caracteres ") }
+            } else {
+                if (_uiState.value.correo == "admin@admin.com") {
+                    _uiState.update { it.copy(mostrarMensajeError = true) }
+                    _uiState.update { it.copy(mensajeError = "Ese correo ya esta en uso ") }
+                } else {
                     viewModelScope.launch {
-                        try {
-                            authRepository.signUp(_uiState.value.correo,_uiState.value.contraseña)
-                           _uiState.update { it.copy(navegar = true) }
-                        }catch (e: Exception){
-                           _uiState.update { it.copy(mostrarMensajeError = true) }
-                           _uiState.update { it.copy(mensajeError = e.message.toString()) }
+
+                        val result =
+                            authRepository.signUp(_uiState.value.correo, _uiState.value.contraseña)
+                        if (result.isSuccess) {
+
+                            _uiState.update { it.copy(navegar = true) }
+                        } else {
+                            val mensaje =
+                                result.exceptionOrNull()?.message ?: "Error al iniciar Sesion"
+                            _uiState.update { it.copy(mostrarMensajeError = true) }
+                            _uiState.update { it.copy(mensajeError = mensaje) }
                         }
+
 
                     }
 
-
                 }
-
             }
         }
     }
-    //funcion para resetear flags
-    fun resetFlag(){
-        _uiState.update { it.copy(navegar = false) }
+        //funcion para resetear flags
+        fun resetFlag() {
+            _uiState.update { it.copy(navegar = false) }
 
-    }
-    //funciones para actualizar los campos de escribir
+        }
+        //funciones para actualizar los campos de escribir
 
-    fun updateCorreo(nuevoCorreo: String) {
-        _uiState.update { it.copy(correo = nuevoCorreo) }
-    }
-    fun updateContraseña(nuevaContraseña: String) {
-        _uiState.update { it.copy(contraseña = nuevaContraseña) }
-    }
-    fun updateUsuario(nuevoUsuario: String) {
-        _uiState.update { it.copy(usuario = nuevoUsuario) }
+        fun updateCorreo(nuevoCorreo: String) {
+            _uiState.update { it.copy(correo = nuevoCorreo) }
+        }
 
+        fun updateContraseña(nuevaContraseña: String) {
+            _uiState.update { it.copy(contraseña = nuevaContraseña) }
+        }
+
+        fun updateUsuario(nuevoUsuario: String) {
+            _uiState.update { it.copy(usuario = nuevoUsuario) }
+
+        }
+
+        fun updateEdad(nuevaEdad: String) {
+            _uiState.update { it.copy(edad = nuevaEdad) }
+        }
+
+        fun updateProfesion(nuevaProfesion: String) {
+            _uiState.update { it.copy(profesion = nuevaProfesion) }
+        }
+
+        fun updateBio(nuevaBio: String) {
+            _uiState.update { it.copy(bio = nuevaBio) }
+        }
     }
-    fun updateEdad(nuevaEdad: String) {
-        _uiState.update { it.copy(edad = nuevaEdad) }
-    }
-    fun updateProfesion(nuevaProfesion: String) {
-        _uiState.update { it.copy(profesion = nuevaProfesion) }
-    }
-    fun updateBio(nuevaBio: String) {
-        _uiState.update { it.copy(bio = nuevaBio) }
-    }
-}
