c73c894ba5cc624199e53bbbcbc60cdb9df2a9d0
jkkhumbgham <juanalbac06@gmail.com>
2025-10-01 20:40:47 -0500

    ServiceDatasourceComment

--- app/src/main/java/com/example/myapplication/data/datasource/ComentarioRemoteDataSource.kt
+++ app/src/main/java/com/example/myapplication/data/datasource/ComentarioRemoteDataSource.kt
@@ -4,7 +4,7 @@ import com.example.myapplication.data.dtos.ComentarioDto
 
 interface ComentarioRemoteDataSource {
     suspend fun getAllCommentarios():List<ComentarioDto>
-    suspend fun getAllComentariosById(id:String): List<ComentarioDto>
+    suspend fun getComentarioById(id:String): ComentarioDto
     suspend fun getAllComentariosByObraId(id: String): List<ComentarioDto>
     suspend fun getAllComentariosByUsuarioId(id: String): List<ComentarioDto>
 }
